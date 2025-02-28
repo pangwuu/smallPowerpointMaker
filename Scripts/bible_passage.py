5f12d7beac788f6f3447bb48b06e58af7d0dabb5
Johnny <panghowu@gmail.com>
2025-03-01 01:16:08 +1100

    Added slides for 3rd march

--- Scripts/bible_passage.py
+++ Scripts/bible_passage.py
@@ -61,12 +61,12 @@ def bible_passage(output_translation="NIV", verse_max=2, newlines_max=4, test_mo
         return parts, verse_reference, version
 
     while True:
-        verse_reference = input("Enter a Bible verse reference such as '2 Peter 1:5-11' (n for template, enter the required version in square brackets if needed e.g. [ESV])\n")
+        verse_reference = input("Enter a Bible verse reference such as '2 Peter 1:5-11' (n for template, enter the required version in brackets if needed e.g. (ESV) )\n")
         
         if verse_reference.lower().strip() == "n":
             return 'T', 'T', output_translation
-        
-        output_translation = re.search("\[(.+)\]", verse_reference).group(1) if re.search("\[(.+)\]", verse_reference) else output_translation
+        pattern = "\((.+)\)"
+        output_translation = re.search(pattern, verse_reference).group(1) if re.search(pattern, verse_reference) else output_translation
 
         verse_reference = verse_reference.replace(f"[{output_translation}]", "").strip()
         
